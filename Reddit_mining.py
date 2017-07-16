5c2f52802d20e7d64ba53b6e4d980ea84ff38745
Stephane Couture <scouture@yorku.ca>
2017-07-16 17:40:01 -0400

    Adding line to create table

--- Reddit_mining.py
+++ Reddit_mining.py
@@ -4,13 +4,12 @@ import sys
 
 subreddit_name = sys.argv[1]
 
-conn = sqlite3.connect('reddit.sqlite')
-c = conn.cursor()
 
 my_subreddit = sys.argv[1]
 my_client_id = sys.argv[2]
 my_client_secret = sys.argv[3]
 my_user_agent = 'Python tryout script'
+
 
 # https://praw.readthedocs.io/en/latest/getting_started/quick_start.html
 reddit = praw.Reddit(client_id=my_client_id,
@@ -19,9 +18,15 @@ reddit = praw.Reddit(client_id=my_client_id,
 
 print(reddit.read_only)  # Output: True
 
+# Open or create the detabase. 
+conn = sqlite3.connect('reddit.sqlite')
+c = conn.cursor()
+
+# Create the table submission
+c.execute("CREATE TABLE submission (Title text, Submission_id integer, User integer, Timestamp datetime, Body text)")
+
 print (my_subreddit);
 all_submissions = reddit.subreddit(my_subreddit).submissions()
-  
       
 for submission in all_submissions:
     # Insert a row of data
