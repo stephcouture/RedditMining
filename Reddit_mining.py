adc6428961c94fe93d70cc1945ee3a2a2f4f35ca
Stephane Couture <scouture@yorku.ca>
2017-07-17 08:12:32 -0400

    Commenting the final version of the script

--- Reddit_mining.py
+++ Reddit_mining.py
@@ -1,3 +1,16 @@
+# Script for Reddit data mining. This script will download datas from a given subreddit account, 
+# and insert them into an sqlite database (reddit.sqlite).
+#
+# Arguments : RedditMining [Name of subreddit] [client_id] [client_secret] 
+# Where [client_id] and [client_secret] can be found on your reddit account.
+#
+# The script will produce a database reddit.sqlite, that you can view (and manipulate with tools
+# such as the SQLITEManager plugin in Firefox : https://addons.mozilla.org/fr/firefox/addon/sqlite-manager/
+# 
+# Created by Stéphane Couture
+# scouture@yorku.ca
+
+
 import praw
 import sqlite3
 import sys
@@ -5,29 +18,31 @@ import sys
 subreddit_name = sys.argv[1]
 
 
-my_subreddit = sys.argv[1]
-my_client_id = sys.argv[2]
-my_client_secret = sys.argv[3]
+# Getting arguments from command line to connect subreddit account
+my_subreddit = sys.argv[1]  # Name of subreddit
+my_client_id = sys.argv[2]  # Client id
+my_client_secret = sys.argv[3] #client secret
 my_user_agent = 'Python tryout script'
 
 
+# Connect to subreddit
 # https://praw.readthedocs.io/en/latest/getting_started/quick_start.html
 reddit = praw.Reddit(client_id=my_client_id,
                      client_secret=my_client_secret,
                      user_agent=my_user_agent)
 
-print(reddit.read_only)  # Output: True
+print(reddit.read_only)  # Output: True is connected
 
 # Open or create the detabase. 
 conn = sqlite3.connect('reddit.sqlite')
 c = conn.cursor()
 
-# Create the table submission and comments, if they do not exists
+# Create the table submission and comment, if they do not exists
 c.execute("CREATE TABLE IF NOT EXISTS submission (Title text, Submission_id string, User string, Timestamp integer, Body text)")
 c.execute("CREATE TABLE IF NOT EXISTS comment (submission_id integer, Comment_id string, Parent_id string, Timestamp integer, User string, Body text)")
 conn.commit() 
 
-#Find the oldest submission here
+#Find the oldest submission here. This is usuful if you want to update the database. More testing needs to be done. 
 c.execute("select min(Timestamp) from submission")
 earliest = c.fetchone()[0]
 print(earliest)
@@ -38,8 +53,8 @@ all_submissions = reddit.subreddit(my_subreddit).submissions(None, earliest)
       
 for submission in all_submissions:
     if ((earliest is None) or (submission.created < earliest)) :  # for some reason, we don't get the earliest.
-        # Insert a row of data
-        #print(vars(submission.author))
+
+        # Insert submission data in the database
         c.execute("INSERT INTO submission (Title, Submission_id, User, Timestamp, Body) \
         VALUES (?,?,?,?,?)", (submission.title,
                               submission.id,
@@ -50,11 +65,13 @@ for submission in all_submissions:
         conn.commit() 
         print(str(submission.created) +" -  id: "+ str(submission.id))
  
+        # Get the comments a the specific submission
         submission.comments.replace_more(limit=0)
         for comment in submission.comments.list():
             # print(vars(comment))  // to test
             # For JSON Reddit, see : https://github.com/reddit/reddit/wiki/JSON
-                    
+            
+            # Put them in the database        
             c.execute("INSERT INTO comment (submission_id, Comment_id, Parent_id, Timestamp, User, Body) \
                 VALUES (?,?,?,?,?, ?)", (submission.id,
                                       comment.id,
